39a1e228d080e456ee1e985616430e78730b8af3
Georg Kullgren <georg@VaioCentOS.(none)>
2013-08-26 08:36:56 +0200

    Use jMockit to mock Calendar

--- test/org/kullgren/timetracker/backend/TimeFunctionsTest.java
+++ test/org/kullgren/timetracker/backend/TimeFunctionsTest.java
@@ -2,13 +2,20 @@ package org.kullgren.timetracker.backend;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import mockit.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import sun.util.resources.CalendarData;
+
 public class TimeFunctionsTest {
 	TimeFunctions func;
-	
+		
 	@Before
 	public void setUp() throws Exception {
 		func = new TimeFunctions();
@@ -24,14 +31,42 @@ public class TimeFunctionsTest {
 	}
 	
 	@Test
-	public void test_stop_running_timer_returns_55_seconds() {
+	public void test_stop_running_timer_returns_55() {
+		final Calendar startTime = Calendar.getInstance();
+		final Calendar endTime = Calendar.getInstance();
+		
+		new Expectations() {
+			Calendar mockCalendar;
+			
+			{
+				Calendar.getInstance(); result = startTime;
+				Calendar.getInstance(); result = endTime;
+				mockCalendar.compareTo(startTime); result = 55;
+			}
+		};
+		
 		func.startTimer();
 		assertEquals(55, func.stopTimer());
 	}
 	
 	@Test
-	public void test_stop_running_timer_returns_5040_seconds() {
+	public void test_stop_running_timer_returns_5040() {
+		final Calendar startTime = Calendar.getInstance();
+		final Calendar endTime = Calendar.getInstance();
+		
+		new Expectations() {
+			Calendar mockCalendar;
+			
+			{
+				Calendar.getInstance(); result = startTime;
+				Calendar.getInstance(); result = endTime;
+				mockCalendar.compareTo(startTime); result = 5040;
+			}
+		};
+		
 		func.startTimer();
 		assertEquals(5040, func.stopTimer());
 	}
 }
+
+
